d6706f2455a5e5a5db7b704bfca1160d430c474a
Germs <anthony.kwy@gmail.com>
2020-09-26 15:44:06 +1000

    randomizer done, fixed recursive error bug

--- randomizer.py
+++ randomizer.py
@@ -5,57 +5,31 @@ from datetime import date
 
 # create class and create methods for each function below
 
-# this function will select a random quote
 def select_random(quotes):
     random_choice = random.choice(quotes)
     return random_choice
 
-# all_quotes = load_quotes() #loads all quotes saved
-# random_quote = select_random(all_quotes) # selects a random quote and stores it
-# random_quote["timestamp"] = time() # updates timestamp to new time
-
-# print(random_quote["text"])
-# print(random_quote["author"])
-
-# save(all_quotes) # saves quotes again into json string
-
-def test_random():
+def test_random(i=0):
+    i += 1
     all_quotes = load_quotes()
     random_quote = select_random(all_quotes)
-    if check_timestamp(random_quote) is True:
+    if check_timestamp(random_quote) is True or i > 150:
         random_quote["timestamp"] = time()
         save(all_quotes)
         return random_quote
     else:
-        test_random()
-    
-# this function will check the timestamp
+        return test_random(i)
+
 def check_timestamp(quote):
     old_timestamp = quote["timestamp"]
     new_timestamp = time()
     month_seconds = 2592000   
     if old_timestamp == None or new_timestamp - old_timestamp > month_seconds:
         return True
-    elif new_timestamp - old_timestamp > month_seconds:
+    elif new_timestamp - old_timestamp < month_seconds:
         return False
 
 print(test_random())
 
 # this function will change the timestamp and update the json file with date
 # def update_timestamp(quote):
-
-
-
-# def select_random():
-#     random_choice = random.choice(load_quotes())
-#     random_choice["timestamp"] = date.today()
-#     choice_quote = random_choice["text"]
-#     choice_author = random_choice["author"]
-#     choice_datestamp = random_choice["timestamp"]
-
-#     print("""{} 
-
-# - {}
-# {}""".format(choice_quote, choice_author, choice_datestamp))
-
-# select_random()
